e0daa02107603a81d283f81475f81728cd76a3c8
usamaajmal <usamaajmal@gmail.com>
2018-07-26 12:16:06 +0500

    Google Maps implemented using API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxGoogleMapModule } from 'ngx-google-map'
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxGoogleMapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
